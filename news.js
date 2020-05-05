720e160c545674cafc23d5b443aeda4ad129db44
Manuel Valtingojer <braunfuchssysstem@gmail.com>
2020-05-05 11:17:21 +0200

    Set environment variables and safe the security

--- news.js
+++ news.js
@@ -1,7 +1,55 @@
-const request = require("request");
-const NEWS_API_KEY = "7d322e19042a418f956c96f0218792d5";
+require('dotenv').config();
+const request = require('request');
+const yargs = require('yargs');
 
-request('https://newsapi.org/v2/top-headlines?country=at&apiKey=7d322e19042a418f956c96f0218792d5', function (error, response, body) {
+var args = yargs
+  .option("c", {
+    describe: "News category of interest",
+    alias: "category",
+    choices: [
+      "business",
+      "entertainment",
+      "general",
+      "health",
+      "science",
+      "sports",
+      "technology",
+    ],
+    type: "string",
+    default: "business",
+    demandOption: "false",
+  })
+  .option("l", {
+    //location
+    describe: 'Country code of interest (e.g., "de", "at", "uk", etc)',
+    alias: "country",
+    type: "string",
+    default: "at",
+    demandOption: "false",
+  })
+  .option("n", {
+    //location
+    describe: 'Country code of interest (e.g., "de", "at", "uk", etc)',
+    alias: "num",
+    type: "number",
+    default: 20,
+    demandOption: "false",
+  })
+  .help("h").argv;
+
+var requestOptions = {
+    url: 'https://newsapi.org/v2/top-headlines',
+    qs: {
+        country: args.country,
+        apiKey: process.env.NEWS_API_KEY,
+        category: args.category,
+        pageSize: args.num
+    }
+};
+
+request(
+    requestOptions,
+    function (error, response, body) {
     
     if (response.statusCode === 200) {
         var bodyObj = JSON.parse(body);
@@ -9,16 +57,7 @@ request('https://newsapi.org/v2/top-headlines?country=at&apiKey=7d322e19042a418f
     
         for (var i = 0; i < bodyObj.articles.length; i++) {
             console.log((i+1) + '. ' + bodyObj.articles[i].title);
+            console.log('   ' + bodyObj.articles[i].url);
         }
     }
 });
-
-/*
-    // Print the error if one occured    
-    console.log('error:', error);
-    // Print the response status code if a response
-    console.log('statusCode:', response && response.statusCode);
-    // Print the HTML for the ecosia homepage
-    console.log('body:', body);
-});
-*/
